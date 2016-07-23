bcc3d62b31b44504638456469b4a2d21ffa8577d
Garcia Mendiburu <amendibu@amazon.com>
2016-07-23 05:55:15 -0700

    Fixed .gitignore, added cross.png, fixed infoWindow from google map

--- static/map.js
+++ static/map.js
@@ -91,20 +91,8 @@ function initMap() {
                  lng: position.coords.longitude
                };
                map.setCenter(pos);
-             }, function() {
-               handleLocationError(true, infoWindow, map.getCenter());
              });
-           } else {
-             // Browser doesn't support Geolocation
-             handleLocationError(false, infoWindow, map.getCenter());
            }
-
-         function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-           infoWindow.setPosition(pos);
-           infoWindow.setContent(browserHasGeolocation ?
-                                 'Error: The Geolocation service failed.' :
-                                 'Error: Your browser doesn\'t support geolocation.');
-         }
 
 	var style_dark = new google.maps.StyledMapType(darkStyle, {name: "Dark"});
 	map.mapTypes.set('dark_style', style_dark);
